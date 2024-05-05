701fc5e463207865f4e52771aff0a9d208f96d05
Terminal123 <138257699+Dark-Ai-py@users.noreply.github.com>
2024-05-05 17:18:59 -0500

    get basic template set up

---
+++ quiz-app/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <nav>
+            <div class="navbar bg-base-100">
+                <div class="flex-1">
+                    <a class="btn btn-ghost text-xl">daisyUI</a>
+                </div>
+                <div class="flex-none gap-2">
+                    <div class="form-control">
+                    <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <div class="options-container">
+        <div class="card w-96 bg-base-100 shadow-xl mt-4">
+            <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="placeholder" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">Math</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
